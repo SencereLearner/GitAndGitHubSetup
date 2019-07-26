b02913cda3a071685671c3c0991f689da6c8d940
SencereLearner <genesisprognosis1156@gmail.com>
2019-07-26 12:09:53 -0500

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import com.sun.tools.javac.util.Assert;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hare Krishna");
+        System.out.println("Hare Rama");
+        String x = "Hare";
+        String y = "Krishna";
+        String z = x + y;
+        System.out.println(z);
     }
 }
